541c59c45d4e277703046e7773b7a9d113e6e5db
squidlywizard <ztdenna@hotmail.com>
2018-11-27 08:22:49 -0700

    developed the bare bones of the panel

--- src/view/frame.java
+++ src/view/frame.java
@@ -1,6 +1,31 @@
 package view;
 
-public class frame
+import javax.swing.JFrame ;
+import control.control;
+import view.panel;
+
+public class frame extends JFrame
 {
+	private control appController;
+	private panel appPanel;
+	
+	public frame(control appController) 
+	{
+		super();
+		
+		this.appController = appController;
+		this.appPanel = new panel(appController);
+		
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(appPanel);
+		this.setSize(1000, 500);
+		this.setTitle("eh I couldnt think of anything");
+		this.setResizable(true);
+		this.setVisible(true);
+	}
 
 }
--- src/view/panel.java
+++ src/view/panel.java
@@ -1,5 +1,35 @@
 package view;
 
-public class panel{
+import control.control;
+import javax.swing.*;
+
+public class panel extends JPanel
+{
+	private control appController;
 	
+	public panel(control appController)
+	{
+		super();
+		this.appController = appController;
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+		
+	}
+	
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
 }
