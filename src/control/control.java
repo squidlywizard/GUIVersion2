18f4694b028cfd13d5d6bf2319d1e1108b258b1c
squidlywizard <ztdenna@hotmail.com>
2018-11-27 08:25:35 -0700

    i changed the controller so we now have a gui

--- src/control/control.java
+++ src/control/control.java
@@ -1,7 +1,14 @@
 package control;
 
+import view.frame;
 public class control
 {
+	private frame appFrame;
+	
+	public control()
+	{
+		this.appFrame = new frame(this);
+	}
 	
 	public void start(){
 		
